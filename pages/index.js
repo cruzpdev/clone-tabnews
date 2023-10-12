4b68da0f695906c05e431228df4428c31b40b1d0
Paulo Cruz <114313554+cruzpdev@users.noreply.github.com>
2023-10-12 20:27:20 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,82 @@
 function Home() {
-    return (
+  return (
     <div>
-        <h1>Ítaca</h1>
-        <p>
-            Quando, de volta, viajares para Ítaca<br />
-            roga que tua rota seja longa,<br />
-            repleta de peripécias, repleta de conhecimentos.<br />
-            Aos Lestrigões, aos Cíclopes,<br />
-            ao colério Posêidon, não temas:<br />
-            tais prodígios jamais encontrará em teu roteiro,<br />
-            se mantiveres altivo o pensamento e seleta<br />
-            a emoção que tocar teu alento e teu corpo.<br />
-            Nem Lestrigões nem Cíclopes,<br />
-            nem o áspero Posêidon encontrarás,<br />
-            se não os tiveres imbuído em teu espírito,<br />
-            se teu espírito não os sucitar diante de si.<br />
-            Roga que sua rota seja longa,<br />
-            que, mútiplas se sucedam as manhãs de verão.<br />
-            Com que euforia, com que júbilo extremo<br />
-            entrarás, pela primeira vez num porto ignoto!<br />
-            Faze escala nos empórios fenícios<br />
-            para arrematar mercadorias belas;<br />
-            madrepérolas e corais, âmbares e ébanos<br />
-            e voluptosas essências aromáticas, várias,<br />
-            tantas essências, tantos arômatas, quantos puderes achar.<br />
-            Detém-te nas cidades do Egito -nas muitas cidades-<br />
-            para aprenderes coisas e mais coisas com os sapientes zelosos.<br />
-            Todo tempo em teu íntimo Ítaca estará presente.<br />
-            Tua sina te assina esse destino,<br />
-            mas não busques apressar sua viagem.<br />
-            É bom que ela tenha uma crônica longa duradoura,<br />
-            que aportes velho, finalmente à ilha,<br />
-            rico do muito que ganhares no decurso do caminho,<br />
-            sem esperares de Ítaca riquezas.<br />
-            Ítaca te deu essa beleza de viagem.<br />
-            Sem ela não a terias empreendido.<br />
-            Nada mais precisa dar-te.<br />
-            Se te parece pobre, Ítaca não te iludiu.<br />
-            Agora tão sábio, tão plenamente vivido,<br />
-            bem compreenderás o sentido das Ítacas.
-        </p>
+      <h1>Ítaca</h1>
+      <p>
+        Quando, de volta, viajares para Ítaca
+        <br />
+        roga que tua rota seja longa,
+        <br />
+        repleta de peripécias, repleta de conhecimentos.
+        <br />
+        Aos Lestrigões, aos Cíclopes,
+        <br />
+        ao colério Posêidon, não temas:
+        <br />
+        tais prodígios jamais encontrará em teu roteiro,
+        <br />
+        se mantiveres altivo o pensamento e seleta
+        <br />
+        a emoção que tocar teu alento e teu corpo.
+        <br />
+        Nem Lestrigões nem Cíclopes,
+        <br />
+        nem o áspero Posêidon encontrarás,
+        <br />
+        se não os tiveres imbuído em teu espírito,
+        <br />
+        se teu espírito não os sucitar diante de si.
+        <br />
+        Roga que sua rota seja longa,
+        <br />
+        que, mútiplas se sucedam as manhãs de verão.
+        <br />
+        Com que euforia, com que júbilo extremo
+        <br />
+        entrarás, pela primeira vez num porto ignoto!
+        <br />
+        Faze escala nos empórios fenícios
+        <br />
+        para arrematar mercadorias belas;
+        <br />
+        madrepérolas e corais, âmbares e ébanos
+        <br />
+        e voluptosas essências aromáticas, várias,
+        <br />
+        tantas essências, tantos arômatas, quantos puderes achar.
+        <br />
+        Detém-te nas cidades do Egito -nas muitas cidades-
+        <br />
+        para aprenderes coisas e mais coisas com os sapientes zelosos.
+        <br />
+        Todo tempo em teu íntimo Ítaca estará presente.
+        <br />
+        Tua sina te assina esse destino,
+        <br />
+        mas não busques apressar sua viagem.
+        <br />
+        É bom que ela tenha uma crônica longa duradoura,
+        <br />
+        que aportes velho, finalmente à ilha,
+        <br />
+        rico do muito que ganhares no decurso do caminho,
+        <br />
+        sem esperares de Ítaca riquezas.
+        <br />
+        Ítaca te deu essa beleza de viagem.
+        <br />
+        Sem ela não a terias empreendido.
+        <br />
+        Nada mais precisa dar-te.
+        <br />
+        Se te parece pobre, Ítaca não te iludiu.
+        <br />
+        Agora tão sábio, tão plenamente vivido,
+        <br />
+        bem compreenderás o sentido das Ítacas.
+      </p>
     </div>
-    );
+  );
 }
 
 export default Home;
